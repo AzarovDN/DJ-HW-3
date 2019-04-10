4ad7881808479085c3636f3a168c5a2674d61de0
AzarovDN <azarovdn@icloud.com>
2019-04-10 14:05:05 +1000

    Сделал 2 класса вместо одного в views.py, сделал отображение дат в пагинации, как в примере.

--- models_list_displaying/books/urls.py
+++ models_list_displaying/books/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
-    path('books/<str:date>', views.BookListView.as_view(), name='sort_books')
+    path('books/<str:date>', views.PaginatorListView.as_view(), name='sort_books')
 
 ]
--- models_list_displaying/books/views.py
+++ models_list_displaying/books/views.py
@@ -6,27 +6,26 @@ from .converters import PubDateConverter
 class BookListView(generic.ListView):
     model = Book
 
+
+class PaginatorListView(BookListView):
+
     def get_context_data(self, **kwargs):
         context = super(BookListView, self).get_context_data(**kwargs)
+        date = self.kwargs["date"]
+        context['filter_date'] = date
+        book_list = list(Book.objects.order_by('pub_date'))
+        date_set = set()
+        converter = PubDateConverter()
 
-        if self.kwargs:
-            date = self.kwargs["date"]
-            context['filter_date'] = date
-            book_list = list(Book.objects.order_by('pub_date'))
-            date_set = set()
-            converter = PubDateConverter()
-            for book in book_list:
-                date_set.add(book.pub_date)
-            date_list = sorted(list(date_set))
+        for book in book_list:
+            date_set.add(book.pub_date)
+        date_list = sorted(list(date_set))
+        index_filter_date = date_list.index(converter.to_python(date))
 
-            index_filter_date = date_list.index(converter.to_python(date))
-            print('дата', date)
-            print(date_list)
-            print('index_filter_date', index_filter_date)
-            if index_filter_date != 0:
-                context['previous_date'] = date_list[index_filter_date - 1]
-            if index_filter_date != (len(date_list) - 1):
-                context['next_date'] = date_list[index_filter_date + 1]
+        if index_filter_date != 0:
+            context['previous_date'] = converter.to_url(date_list[index_filter_date - 1])
+        if index_filter_date != (len(date_list) - 1):
+            context['next_date'] = converter.to_url(date_list[index_filter_date + 1])
 
         return context
 
@@ -36,5 +35,7 @@ class BookListView(generic.ListView):
             return Book.objects.filter(pub_date=f'{self.kwargs["date"]}')
 
         return Book.objects.all()
+
+    pass
 
 
